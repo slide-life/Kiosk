bcc770ff5d013513bc5f1ee5d12ed6b4183c2c4b
Matt Neary <neary.matt@gmail.com>
2014-12-09 22:58:36 -0500

    Initial Commit

---
+++ SlideKiosk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideKiosk
+//
+//  Created by Matt Neary on 12/9/14.
+//  Copyright (c) 2014 slide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideKiosk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideKiosk
+//
+//  Created by Matt Neary on 12/9/14.
+//  Copyright (c) 2014 slide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
